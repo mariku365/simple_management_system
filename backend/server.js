cf2c8c1df7b0aeaca94cc9264aa5791df739b47f
mariku365 <markkg3016@gmail.com>
2026-09-09 01:36:23 +0800

    Finished the CRUD operations for the backend

--- backend/server.js
+++ backend/server.js
@@ -47,4 +47,58 @@ app.post("/api/login", async (req, res) => {
     }
 })
 
+app.post("/api/items", async (req, res) => {
+    const { name, quantity, price } = req.body;
+
+    try {
+        await sql.connect(dbConfig);
+        await sql.query`
+            INSERT INTO items (itemName, quantity, price)
+            VALUES (${name}, ${quantity}, ${price})        
+        `;
+        res.json({ success: true, message: `Successfully added the item: ${name}`})
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+app.get("/api/items", async (req, res) => {
+    try {
+        await sql.connect(dbConfig);
+        const result = await sql.query`SELECT * FROM items`;
+        res.json(result.recordset);
+    } catch (err) {
+        res.status(500).json({message: "err.message"});
+    };
+});
+
+app.put("/api/items/:id", async(req, res) => {
+    const { id } = req.params;
+    const { itemName, quantity, price } = req.body;
+
+    try {
+        await sql.connect(dbConfig);
+        await sql.query`
+            UPDATE items
+            SET itemName = ${itemName}, quantity = ${quantity}, price = ${price}
+            WHERE itemId = ${id}
+        `;
+        res.json({success: true, message: "Item successfully updated"})
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+app.delete("/api/items/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await sql.connect(dbConfig);
+        await sql.query`DELETE FROM items WHERE itemId = ${id}`;
+        res.json({success: true, message: "Item deleted successfully"});
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    };
+})
+
+
 app.listen(3001, () => console.log("Server running on port 3001"));
